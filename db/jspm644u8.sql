ba0733533a94791d14ded8fef63da5c2969fbc0d
yulinyu <2508000341@QQ.COM>
2025-03-14 10:37:25 +0800

    1

---
+++ db/jspm644u8.sql
@@ -0,0 +1,444 @@
+-- MySQL dump 10.13  Distrib 5.7.31, for Linux (x86_64)
+--
+-- Host: localhost    Database: jspm644u8
+-- ------------------------------------------------------
+-- Server version	5.7.31
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `address`
+--
+
+DROP TABLE IF EXISTS `address`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `address` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `addtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `userid` bigint(20) NOT NULL COMMENT '用户id',
+  `address` varchar(200) NOT NULL COMMENT '地址',
+  `name` varchar(200) NOT NULL COMMENT '收货人',
+  `phone` varchar(200) NOT NULL COMMENT '电话',
+  `isdefault` varchar(200) NOT NULL COMMENT '是否默认地址[是/否]',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COMMENT='地址';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `address`
+--
+
+LOCK TABLES `address` WRITE;
+/*!40000 ALTER TABLE `address` DISABLE KEYS */;
+INSERT INTO `address` VALUES (1,'2021-02-01 03:12:19',1,'宇宙银河系金星1号','金某','13823888881','是'),(2,'2021-02-01 03:12:19',2,'宇宙银河系木星1号','木某','13823888882','是'),(3,'2021-02-01 03:12:19',3,'宇宙银河系水星1号','水某','13823888883','是'),(4,'2021-02-01 03:12:19',4,'宇宙银河系火星1号','火某','13823888884','是'),(5,'2021-02-01 03:12:19',5,'宇宙银河系土星1号','土某','13823888885','是'),(6,'2021-02-01 03:12:19',6,'宇宙银河系月球1号','月某','13823888886','是');
+/*!40000 ALTER TABLE `address` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cart`
+--
+
+DROP TABLE IF EXISTS `cart`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cart` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `addtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `tablename` varchar(200) DEFAULT 'shangpinxinxi' COMMENT '商品表名',
+  `userid` bigint(20) NOT NULL COMMENT '用户id',
+  `goodid` bigint(20) NOT NULL COMMENT '商品id',
+  `goodname` varchar(200) DEFAULT NULL COMMENT '商品名称',
+  `picture` varchar(200) DEFAULT NULL COMMENT '图片',
+  `buynumber` int(11) NOT NULL COMMENT '购买数量',
+  `price` float DEFAULT NULL COMMENT '单价',
+  `discountprice` float DEFAULT NULL COMMENT '会员价',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='购物车表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cart`
+--
+
+LOCK TABLES `cart` WRITE;
+/*!40000 ALTER TABLE `cart` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cart` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `config`
+--
+
+DROP TABLE IF EXISTS `config`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `config` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `name` varchar(100) NOT NULL COMMENT '配置参数名称',
+  `value` varchar(100) DEFAULT NULL COMMENT '配置参数值',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COMMENT='配置文件';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `config`
+--
+
+LOCK TABLES `config` WRITE;
+/*!40000 ALTER TABLE `config` DISABLE KEYS */;
+INSERT INTO `config` VALUES (1,'picture1','http://localhost:8080/jspm644u8/upload/picture1.jpg'),(2,'picture2','http://localhost:8080/jspm644u8/upload/picture2.jpg'),(3,'picture3','http://localhost:8080/jspm644u8/upload/picture3.jpg'),(6,'homepage',NULL);
+/*!40000 ALTER TABLE `config` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `discussshangpinxinxi`
+--
+
+DROP TABLE IF EXISTS `discussshangpinxinxi`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `discussshangpinxinxi` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `addtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `refid` bigint(20) NOT NULL COMMENT '关联表id',
+  `userid` bigint(20) NOT NULL COMMENT '用户id',
+  `content` longtext NOT NULL COMMENT '评论内容',
+  `reply` longtext COMMENT '回复内容',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=117 DEFAULT CHARSET=utf8 COMMENT='商品信息评论表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `discussshangpinxinxi`
+--
+
+LOCK TABLES `discussshangpinxinxi` WRITE;
+/*!40000 ALTER TABLE `discussshangpinxinxi` DISABLE KEYS */;
+INSERT INTO `discussshangpinxinxi` VALUES (111,'2021-02-01 03:12:19',1,1,'评论内容1','回复内容1'),(112,'2021-02-01 03:12:19',2,2,'评论内容2','回复内容2'),(113,'2021-02-01 03:12:19',3,3,'评论内容3','回复内容3'),(114,'2021-02-01 03:12:19',4,4,'评论内容4','回复内容4'),(115,'2021-02-01 03:12:19',5,5,'评论内容5','回复内容5'),(116,'2021-02-01 03:12:19',6,6,'评论内容6','回复内容6');
+/*!40000 ALTER TABLE `discussshangpinxinxi` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `forum`
+--
+
+DROP TABLE IF EXISTS `forum`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `forum` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `addtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `title` varchar(200) DEFAULT NULL COMMENT '帖子标题',
+  `content` longtext NOT NULL COMMENT '帖子内容',
+  `parentid` bigint(20) DEFAULT NULL COMMENT '父节点id',
+  `userid` bigint(20) NOT NULL COMMENT '用户id',
+  `username` varchar(200) DEFAULT NULL COMMENT '用户名',
+  `isdone` varchar(200) DEFAULT NULL COMMENT '状态',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=47 DEFAULT CHARSET=utf8 COMMENT='成长交流';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `forum`
+--
+
+LOCK TABLES `forum` WRITE;
+/*!40000 ALTER TABLE `forum` DISABLE KEYS */;
+INSERT INTO `forum` VALUES (41,'2021-02-01 03:12:19','帖子标题1','帖子内容1',1,1,'用户名1','开放'),(42,'2021-02-01 03:12:19','帖子标题2','帖子内容2',2,2,'用户名2','开放'),(43,'2021-02-01 03:12:19','帖子标题3','帖子内容3',3,3,'用户名3','开放'),(44,'2021-02-01 03:12:19','帖子标题4','帖子内容4',4,4,'用户名4','开放'),(45,'2021-02-01 03:12:19','帖子标题5','帖子内容5',5,5,'用户名5','开放'),(46,'2021-02-01 03:12:19','帖子标题6','帖子内容6',6,6,'用户名6','开放');
+/*!40000 ALTER TABLE `forum` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `messages`
+--
+
+DROP TABLE IF EXISTS `messages`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `messages` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `addtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `userid` bigint(20) NOT NULL COMMENT '留言人id',
+  `username` varchar(200) DEFAULT NULL COMMENT '用户名',
+  `content` longtext NOT NULL COMMENT '留言内容',
+  `reply` longtext COMMENT '回复内容',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=107 DEFAULT CHARSET=utf8 COMMENT='留言板';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `messages`
+--
+
+LOCK TABLES `messages` WRITE;
+/*!40000 ALTER TABLE `messages` DISABLE KEYS */;
+INSERT INTO `messages` VALUES (101,'2021-02-01 03:12:19',1,'用户名1','留言内容1','回复内容1'),(102,'2021-02-01 03:12:19',2,'用户名2','留言内容2','回复内容2'),(103,'2021-02-01 03:12:19',3,'用户名3','留言内容3','回复内容3'),(104,'2021-02-01 03:12:19',4,'用户名4','留言内容4','回复内容4'),(105,'2021-02-01 03:12:19',5,'用户名5','留言内容5','回复内容5'),(106,'2021-02-01 03:12:19',6,'用户名6','留言内容6','回复内容6');
+/*!40000 ALTER TABLE `messages` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `news`
+--
+
+DROP TABLE IF EXISTS `news`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `news` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `addtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `title` varchar(200) NOT NULL COMMENT '标题',
+  `introduction` longtext COMMENT '简介',
+  `picture` varchar(200) NOT NULL COMMENT '图片',
+  `content` longtext NOT NULL COMMENT '内容',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=97 DEFAULT CHARSET=utf8 COMMENT='系统公告';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `news`
+--
+
+LOCK TABLES `news` WRITE;
+/*!40000 ALTER TABLE `news` DISABLE KEYS */;
+INSERT INTO `news` VALUES (91,'2021-02-01 03:12:19','标题1','简介1','http://localhost:8080/jspm644u8/upload/news_picture1.jpg','内容1'),(92,'2021-02-01 03:12:19','标题2','简介2','http://localhost:8080/jspm644u8/upload/news_picture2.jpg','内容2'),(93,'2021-02-01 03:12:19','标题3','简介3','http://localhost:8080/jspm644u8/upload/news_picture3.jpg','内容3'),(94,'2021-02-01 03:12:19','标题4','简介4','http://localhost:8080/jspm644u8/upload/news_picture4.jpg','内容4'),(95,'2021-02-01 03:12:19','标题5','简介5','http://localhost:8080/jspm644u8/upload/news_picture5.jpg','内容5'),(96,'2021-02-01 03:12:19','标题6','简介6','http://localhost:8080/jspm644u8/upload/news_picture6.jpg','内容6');
+/*!40000 ALTER TABLE `news` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `orders` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `addtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `orderid` varchar(200) NOT NULL COMMENT '订单编号',
+  `tablename` varchar(200) DEFAULT 'shangpinxinxi' COMMENT '商品表名',
+  `userid` bigint(20) NOT NULL COMMENT '用户id',
+  `goodid` bigint(20) NOT NULL COMMENT '商品id',
+  `goodname` varchar(200) DEFAULT NULL COMMENT '商品名称',
+  `picture` varchar(200) DEFAULT NULL COMMENT '商品图片',
+  `buynumber` int(11) NOT NULL COMMENT '购买数量',
+  `price` float NOT NULL DEFAULT '0' COMMENT '价格/积分',
+  `discountprice` float DEFAULT '0' COMMENT '折扣价格',
+  `total` float NOT NULL DEFAULT '0' COMMENT '总价格/总积分',
+  `discounttotal` float DEFAULT '0' COMMENT '折扣总价格',
+  `type` int(11) DEFAULT '1' COMMENT '支付类型',
+  `status` varchar(200) DEFAULT NULL COMMENT '状态',
+  `address` varchar(200) DEFAULT NULL COMMENT '地址',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `orderid` (`orderid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='订单';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders`
+--
+
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `shangpinfenlei`
+--
+
+DROP TABLE IF EXISTS `shangpinfenlei`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `shangpinfenlei` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `addtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `shangpinleibie` varchar(200) DEFAULT NULL COMMENT '商品类别',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8 COMMENT='商品分类';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `shangpinfenlei`
+--
+
+LOCK TABLES `shangpinfenlei` WRITE;
+/*!40000 ALTER TABLE `shangpinfenlei` DISABLE KEYS */;
+INSERT INTO `shangpinfenlei` VALUES (21,'2021-02-01 03:12:19','商品类别1'),(22,'2021-02-01 03:12:19','商品类别2'),(23,'2021-02-01 03:12:19','商品类别3'),(24,'2021-02-01 03:12:19','商品类别4'),(25,'2021-02-01 03:12:19','商品类别5'),(26,'2021-02-01 03:12:19','商品类别6');
+/*!40000 ALTER TABLE `shangpinfenlei` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `shangpinxinxi`
+--
+
+DROP TABLE IF EXISTS `shangpinxinxi`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `shangpinxinxi` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `addtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `shangpinbianhao` varchar(200) DEFAULT NULL COMMENT '商品编号',
+  `shangpinmingcheng` varchar(200) DEFAULT NULL COMMENT '商品名称',
+  `shangpinleibie` varchar(200) DEFAULT NULL COMMENT '商品类别',
+  `shangpinguige` varchar(200) DEFAULT NULL COMMENT '商品规格',
+  `tupian` varchar(200) DEFAULT NULL COMMENT '图片',
+  `pinpai` varchar(200) DEFAULT NULL COMMENT '品牌',
+  `shangpinjieshao` longtext COMMENT '商品介绍',
+  `thumbsupnum` int(11) DEFAULT '0' COMMENT '赞',
+  `crazilynum` int(11) DEFAULT '0' COMMENT '踩',
+  `jifen` int(11) DEFAULT '-1' COMMENT '积分',
+  `price` float NOT NULL COMMENT '价格',
+  `onelimittimes` int(11) DEFAULT '-1' COMMENT '单限',
+  `alllimittimes` int(11) DEFAULT '-1' COMMENT '库存',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `shangpinbianhao` (`shangpinbianhao`)
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8 COMMENT='商品信息';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `shangpinxinxi`
+--
+
+LOCK TABLES `shangpinxinxi` WRITE;
+/*!40000 ALTER TABLE `shangpinxinxi` DISABLE KEYS */;
+INSERT INTO `shangpinxinxi` VALUES (31,'2021-02-01 03:12:19','商品编号1','商品名称1','商品类别1','商品规格1','http://localhost:8080/jspm644u8/upload/shangpinxinxi_tupian1.jpg','品牌1','商品介绍1',1,1,10000,99.9,1,99),(32,'2021-02-01 03:12:19','商品编号2','商品名称2','商品类别2','商品规格2','http://localhost:8080/jspm644u8/upload/shangpinxinxi_tupian2.jpg','品牌2','商品介绍2',2,2,10000,99.9,2,99),(33,'2021-02-01 03:12:19','商品编号3','商品名称3','商品类别3','商品规格3','http://localhost:8080/jspm644u8/upload/shangpinxinxi_tupian3.jpg','品牌3','商品介绍3',3,3,10000,99.9,3,99),(34,'2021-02-01 03:12:19','商品编号4','商品名称4','商品类别4','商品规格4','http://localhost:8080/jspm644u8/upload/shangpinxinxi_tupian4.jpg','品牌4','商品介绍4',4,4,10000,99.9,4,99),(35,'2021-02-01 03:12:19','商品编号5','商品名称5','商品类别5','商品规格5','http://localhost:8080/jspm644u8/upload/shangpinxinxi_tupian5.jpg','品牌5','商品介绍5',5,5,10000,99.9,5,99),(36,'2021-02-01 03:12:19','商品编号6','商品名称6','商品类别6','商品规格6','http://localhost:8080/jspm644u8/upload/shangpinxinxi_tupian6.jpg','品牌6','商品介绍6',6,6,10000,99.9,6,99);
+/*!40000 ALTER TABLE `shangpinxinxi` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `storeup`
+--
+
+DROP TABLE IF EXISTS `storeup`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `storeup` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `addtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `userid` bigint(20) NOT NULL COMMENT '用户id',
+  `refid` bigint(20) DEFAULT NULL COMMENT '收藏id',
+  `tablename` varchar(200) DEFAULT NULL COMMENT '表名',
+  `name` varchar(200) NOT NULL COMMENT '收藏名称',
+  `picture` varchar(200) NOT NULL COMMENT '收藏图片',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='收藏表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `storeup`
+--
+
+LOCK TABLES `storeup` WRITE;
+/*!40000 ALTER TABLE `storeup` DISABLE KEYS */;
+/*!40000 ALTER TABLE `storeup` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `token`
+--
+
+DROP TABLE IF EXISTS `token`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `token` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `userid` bigint(20) NOT NULL COMMENT '用户id',
+  `username` varchar(100) NOT NULL COMMENT '用户名',
+  `tablename` varchar(100) DEFAULT NULL COMMENT '表名',
+  `role` varchar(100) DEFAULT NULL COMMENT '角色',
+  `token` varchar(200) NOT NULL COMMENT '密码',
+  `addtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '新增时间',
+  `expiratedtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '过期时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='token表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `token`
+--
+
+LOCK TABLES `token` WRITE;
+/*!40000 ALTER TABLE `token` DISABLE KEYS */;
+/*!40000 ALTER TABLE `token` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `username` varchar(100) NOT NULL COMMENT '用户名',
+  `password` varchar(100) NOT NULL COMMENT '密码',
+  `role` varchar(100) DEFAULT '管理员' COMMENT '角色',
+  `addtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '新增时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='用户表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'abo','abo','管理员','2021-02-01 03:12:19');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `yonghu`
+--
+
+DROP TABLE IF EXISTS `yonghu`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `yonghu` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `addtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `yonghuming` varchar(200) NOT NULL COMMENT '用户名',
+  `mima` varchar(200) DEFAULT NULL COMMENT '密码',
+  `xingming` varchar(200) DEFAULT NULL COMMENT '姓名',
+  `nianling` varchar(200) DEFAULT NULL COMMENT '年龄',
+  `shouji` varchar(200) DEFAULT NULL COMMENT '手机',
+  `youxiang` varchar(200) DEFAULT NULL COMMENT '邮箱',
+  `jifen` int(11) DEFAULT '0' COMMENT '积分',
+  `money` float DEFAULT '0' COMMENT '余额',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `yonghuming` (`yonghuming`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8 COMMENT='用户';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `yonghu`
+--
+
+LOCK TABLES `yonghu` WRITE;
+/*!40000 ALTER TABLE `yonghu` DISABLE KEYS */;
+INSERT INTO `yonghu` VALUES (11,'2021-02-01 03:12:19','用户1','123456','姓名1','年龄1','13823888881','773890001@qq.com',10000,100),(12,'2021-02-01 03:12:19','用户2','123456','姓名2','年龄2','13823888882','773890002@qq.com',10000,100),(13,'2021-02-01 03:12:19','用户3','123456','姓名3','年龄3','13823888883','773890003@qq.com',10000,100),(14,'2021-02-01 03:12:19','用户4','123456','姓名4','年龄4','13823888884','773890004@qq.com',10000,100),(15,'2021-02-01 03:12:19','用户5','123456','姓名5','年龄5','13823888885','773890005@qq.com',10000,100),(16,'2021-02-01 03:12:19','用户6','123456','姓名6','年龄6','13823888886','773890006@qq.com',10000,100);
+/*!40000 ALTER TABLE `yonghu` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-02-02 10:49:05
